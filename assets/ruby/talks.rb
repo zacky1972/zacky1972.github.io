f49a29446bde9d83e41ba9ea6429e7ca56d20ccd
zacky1972 <zacky1972@gmail.com>
2015-12-29 20:16:22 +0900

    revise Podcast

--- assets/ruby/talks.rb
+++ assets/ruby/talks.rb
@@ -46,7 +46,7 @@ def renderPodcast(talks)
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
   <channel>
     <title>{{ title }}</title>
-    <link>{{ link }}</title>
+    <link>{{ link }}</link>
     <language>{{ language }}</language>
     <copyright>&#xA9; {{ author }} </copyright>
     <itunes:subtitle>{{ summary }}</itunes:subtitle>
