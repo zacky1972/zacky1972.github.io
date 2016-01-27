e011829de42e9d93679b8169cc606dd831bc8a17
zacky1972 <zacky1972@gmail.com>
2016-01-28 06:15:56 +0900

    add comment of specification

--- assets/javascripts/common.js
+++ assets/javascripts/common.js
@@ -1,4 +1,6 @@
 /*! common.js for zacky1972.github.io copyright Susumu Yamazaki */
+
+/* record url to Google Analytics when a hyperlink to another web site is clicked */ 
 $(document).ready(function(){
 	$("a").click(function () {
 		var url = $(this).attr("href");
