766805d9c89426ff23b2e4f73a23f660913bf99c
zacky1972 <zacky1972@gmail.com>
2016-01-28 06:05:05 +0900

    add function that record GA when clicked on href

--- assets/javascripts/common.js
+++ assets/javascripts/common.js
@@ -1,6 +1,6 @@
 /*! common.js for zacky1972.github.io copyright Susumu Yamazaki */
 $(document).ready(function(){
 	$("a").click(function () {
-		$(this).fadeOut("slow"); 
+		ga('send', 'pageview', {'page': $(this).attr("href"), 'title':$(this).attr("href")}); 
 	});
 });
