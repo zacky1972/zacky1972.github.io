76cfed61ea5d7d6a0d6e4d22bd762d51c7017d8f
zacky1972 <zacky1972@gmail.com>
2017-10-07 05:10:44 +0900

    revise a exerceise: price

--- assets/src/product.cpp
+++ assets/src/product.cpp
@@ -9,6 +9,11 @@ class Product
 protected:
     int price;
 public:
+    void SetPrice(int p)
+    {
+        this->price = p;
+    }
+
     virtual int GetPrice()
     {
         return price;
@@ -16,3 +21,11 @@ public:
 
     virtual ~Product() {}
 };
+
+int main()
+{
+    Product* product = new Product();
+    product->SetPrice(100);
+    cout << "商品の価格は" << product->GetPrice() << "円" << endl;
+    return 0;
+}
